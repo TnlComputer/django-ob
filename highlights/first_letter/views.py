9d7d0d6938f9de085a8fa52c426833f3ad2ffea3
Jorge Martinez <tnlcomputer@gmail.com>
2023-03-21 15:48:22 -0300

    finalizacion del proyecto y fin de curso

--- highlights/first_letter/views.py
+++ highlights/first_letter/views.py
@@ -4,4 +4,4 @@ from .models import Product
 
 def first(request, letter):
     products = Product.objects.filter(name__istartswith=letter)
-    return render(request, 'first.html', {products: products})
+    return render(request, 'first.html', {'products': products})
